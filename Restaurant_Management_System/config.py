19f5c62a682c1afcc95be2f0e08d48082451d52e
Priyadarshini Katepalli <pk755@njit.edu>
2026-07-16 17:37:13 -0400

    Delete Restaurant_Management_System directory

--- Restaurant_Management_System/config.py
+++
@@ -1,8 +0,0 @@
-MENU_FILE = "data/menu.json"
-ORDERS_FILE = "data/orders.json"
-SALES_FILE = "data/sales.json"
-STOCK_FILE = "data/stock.json"
-REPORTS_DIR = "reports/"
-INVOICE_DIR = "reports/invoices/"
-TAX_RATE = 0.05
-CURRENCY_SYMBOL = "$"
